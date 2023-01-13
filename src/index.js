49d9d5883c9bac8d2cf0f18cde3f2140ef2b68f0
Saharsh Gupta <saharshg29@gmail.com>
2023-01-13 15:51:14 +0530

    Login page working fine

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+ // eslint-disable-next-line
 import $ from "jquery";
+ // eslint-disable-next-line
 import Popper from "popper.js";
 import { BrowserRouter as Browser } from "react-router-dom";
 
